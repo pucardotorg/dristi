56d09baff5b0d7635bba737d2f421829b6b02392
ShivamAggarwal4 <shivam.a@beehyv.com>
2024-04-04 13:13:29 +0530

    advocate clerk create api changes

---
+++ backend/advocate/src/main/java/org/pucar/enrichment/AdvocateClerkRegistrationEnrichment.java
@@ -0,0 +1,46 @@
+package org.pucar.enrichment;
+
+
+        import lombok.extern.slf4j.Slf4j;
+        import org.egov.common.contract.models.AuditDetails;
+        import org.pucar.util.IdgenUtil;
+        import org.pucar.util.UserUtil;
+        import org.pucar.web.models.AdvocateClerk;
+        import org.pucar.web.models.AdvocateClerkRequest;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Component;
+
+        import java.util.List;
+        import java.util.UUID;
+
+@Component
+@Slf4j
+public class AdvocateClerkRegistrationEnrichment {
+
+    @Autowired
+    private IdgenUtil idgenUtil;
+
+    @Autowired
+    private UserUtil userUtils;
+
+    public void enrichAdvocateClerkRegistration(AdvocateClerkRequest advocateClerkRequest) {
+        try {
+            List<String> advocateClerkRegistrationIdList = idgenUtil.getIdList(advocateClerkRequest.getRequestInfo(), advocateClerkRequest.getClerks().get(0).getTenantId(), "product.id", "P-[cy:yyyy-MM-dd]-[SEQ_PRODUCT_P]", advocateClerkRequest.getClerks().size());
+            Integer index = 0;
+            for(AdvocateClerk advocateClerk : advocateClerkRequest.getClerks()){
+                // Enrich audit details
+                AuditDetails auditDetails = AuditDetails.builder().createdBy(advocateClerkRequest.getRequestInfo().getUserInfo().getUuid()).createdTime(System.currentTimeMillis()).lastModifiedBy(advocateClerkRequest.getRequestInfo().getUserInfo().getUuid()).lastModifiedTime(System.currentTimeMillis()).build();
+                advocateClerk.setAuditDetails(auditDetails);
+
+                // Enrich UUID
+                advocateClerk.setId(UUID.randomUUID());
+
+                //Enrich application number from IDgen
+                advocateClerk.setApplicationNumber(advocateClerkRegistrationIdList.get(index++));
+            }
+        } catch (Exception e) {
+            log.error("Error enriching birth application: {}", e.getMessage());
+            // Handle the exception or throw a custom exception
+        }
+    }
+}
