4e196f6ba19c552b87ca5eed9c1befc46e23ada9
manimaarans <manimaaran@beehyv.com>
2024-04-22 00:08:03 +0530

    fix test cases

--- backend/advocate/src/test/java/org/pucar/web/controllers/AdvocateApiControllerTest.java
+++ backend/advocate/src/test/java/org/pucar/web/controllers/AdvocateApiControllerTest.java
@@ -105,7 +105,7 @@ public class AdvocateApiControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(advocateRequest)))
-                        .andExpect(status().isInternalServerError());
+                        .andExpect(status().isBadRequest());
     }
 
 //    @Test
@@ -131,6 +131,6 @@ public class AdvocateApiControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(advocateSearchRequest)))
-                .andExpect(status().isInternalServerError());
+                .andExpect(status().isBadRequest());
     }
 }
