34077b4094e44dba461a441490bcf1d722966c20
Suresh Soren <sureshsoren@beehyv.com>
2024-05-10 21:59:09 +0530

    FE del

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/Login/SelectName.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import FormStep from "../../../components/FormStep";
-
-const SelectName = ({ config, onSelect, t, isDisabled }) => {
-  return <FormStep config={config} onSelect={onSelect} t={t} isDisabled={isDisabled} cardStyle={{ minWidth: "100%" }}></FormStep>;
-};
-
-export default SelectName;
