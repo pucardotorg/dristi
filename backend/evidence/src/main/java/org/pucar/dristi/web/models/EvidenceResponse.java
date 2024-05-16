d6dbd0d11a0c41d44ce83e2a0e829877869bf207
Subhashini Srinivasan <subhashini.srinivasan@egovernments.org>
2024-05-16 16:39:59 +0530

    Initial codegen for evidence service

---
+++ backend/evidence/src/main/java/org/pucar/dristi/web/models/EvidenceResponse.java
@@ -0,0 +1,49 @@
+package org.pucar.dristi.web.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.egov.common.contract.response.ResponseInfo;
+import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * EvidenceResponse
+ */
+@Validated
+@jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-05-16T15:17:16.225735+05:30[Asia/Kolkata]")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EvidenceResponse {
+	@JsonProperty("responseInfo")
+
+	@Valid
+	private ResponseInfo responseInfo = null;
+
+	@JsonProperty("artifacts")
+	@Valid
+	private List<Artifact> artifacts = null;
+
+	@JsonProperty("pagination")
+
+	@Valid
+	private Pagination pagination = null;
+
+	public EvidenceResponse addArtifactsItem(Artifact artifactsItem) {
+		if (this.artifacts == null) {
+			this.artifacts = new ArrayList<>();
+		}
+		this.artifacts.add(artifactsItem);
+		return this;
+	}
+
+}
