3f72885d9c07e195124d6a0d036f30f84eb113d1
K Anirudh <anirudh.k@beehyv.com>
2024-05-17 13:15:17 +0530

    Cheque Details v1

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/index.js
@@ -2,9 +2,10 @@ import { AppContainer } from "@egovernments/digit-ui-react-components";
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
 import CaseType from "./CaseType";
-import RespondentDetails from "./RespondentDetails";
-import DelayApplication from "./DelayApplication";
+import ChequeDetails from "./ChequeDetails";
 import ComplainantDetails from "./ComplainantDetails";
+import DelayApplication from "./DelayApplication";
+import RespondentDetails from "./RespondentDetails";
 
 function FileCase({ t }) {
   const { path } = useRouteMatch();
@@ -24,6 +25,9 @@ function FileCase({ t }) {
           </Route>
           <Route path={`${path}/complainant-details`} exact>
             <ComplainantDetails t={t} />
+          </Route>
+          <Route path={`${path}/cheque-details`} exact>
+            <ChequeDetails />
           </Route>
         </AppContainer>
       </Switch>
