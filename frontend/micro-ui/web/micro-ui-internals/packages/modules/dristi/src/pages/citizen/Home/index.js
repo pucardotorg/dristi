a78605ae4b76f71b9dcd4044b4d981416ad389c8
Suresh Soren <sureshsoren@beehyv.com>
2024-05-18 19:54:42 +0530

    route fix for case file

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/Home/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/Home/index.js
@@ -94,7 +94,18 @@ function CitizenHome({ tenantId }) {
         !isApprovalPending &&
         !isRejected &&
         cardIcons.map((card) => {
-          return <CustomCard label={card.label} Icon={card.Icon} style={{ width: "400px", height: "150px" }} onClick={() => {}}></CustomCard>;
+          return (
+            <CustomCard
+              label={card.label}
+              Icon={card.Icon}
+              style={{ width: "400px", height: "150px" }}
+              onClick={() => {
+                if (card.label === "File a Case") {
+                  history.push(card.path);
+                }
+              }}
+            ></CustomCard>
+          );
         })}
       {individualId && isApprovalPending && !isRejected && <ApplicationAwaitingPage individualId={individualId} />}
       {(!individualId || isRejected) && <TakeUserToRegistration message={isRejected ? "CS_REJECT_MESSAGE" : "CS_REGISTRATION_MESSAGE"} />}
