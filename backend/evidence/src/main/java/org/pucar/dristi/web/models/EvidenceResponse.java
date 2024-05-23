ef9b396096e123db211b161457e3f53ab1641309
ShivamAggarwal4 <shivam.a@beehyv.com>
2024-05-23 15:14:10 +0530

    responseinfo r capital

--- backend/evidence/src/main/java/org/pucar/dristi/web/models/EvidenceResponse.java
+++ backend/evidence/src/main/java/org/pucar/dristi/web/models/EvidenceResponse.java
@@ -24,7 +24,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class EvidenceResponse {
-	@JsonProperty("responseInfo")
+	@JsonProperty("ResponseInfo")
 
 	@Valid
 	private ResponseInfo responseInfo = null;
