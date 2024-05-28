ab7f4c1f857b9146911e0cbdc92c29cb0ebaadda
ShivamAggarwal4 <shivam.a@beehyv.com>
2024-05-28 15:44:14 +0530

    testcases evidence service addition

--- backend/evidence/src/test/java/org/pucar/dristi/service/EvidenceServiceTest.java
+++ backend/evidence/src/test/java/org/pucar/dristi/service/EvidenceServiceTest.java
@@ -1,5 +1,6 @@
 package org.pucar.dristi.service;
 
+import org.egov.tracer.model.CustomException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,8 +14,8 @@ import org.pucar.dristi.web.models.Artifact;
 import org.pucar.dristi.web.models.EvidenceRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class EvidenceServiceTest {
 
@@ -55,7 +56,41 @@ public class EvidenceServiceTest {
         verify(workflowService).updateWorkflowStatus(evidenceRequest);
         verify(producer).push("create_topic", evidenceRequest);
     }
+    @Test
+    public void testCreateEvidenceWithCustomException() {
+        // Prepare data
+        EvidenceRequest evidenceRequest = new EvidenceRequest();
+        evidenceRequest.setArtifact(new Artifact());
 
+        // Mock behavior
+        when(config.getEvidenceCreateTopic()).thenReturn("create_topic");
+        // Mocking validation throwing CustomException
+        doThrow(new CustomException()).when(validator).validateEvidenceRegistration(evidenceRequest);
+
+        // Execute and assert
+        assertThrows(CustomException.class, () -> evidenceService.createEvidence(evidenceRequest));
+        // Verify behavior
+        verify(validator).validateEvidenceRegistration(evidenceRequest);
+        verifyNoInteractions(workflowService, producer);
+    }
+
+    @Test
+    public void testCreateEvidenceWithOtherException() {
+        // Prepare data
+        EvidenceRequest evidenceRequest = new EvidenceRequest();
+        evidenceRequest.setArtifact(new Artifact());
+
+        // Mock behavior
+        when(config.getEvidenceCreateTopic()).thenReturn("create_topic");
+        // Mocking validation throwing RuntimeException
+        doThrow(new RuntimeException()).when(validator).validateEvidenceRegistration(evidenceRequest);
+
+        // Execute and assert
+        assertThrows(CustomException.class, () -> evidenceService.createEvidence(evidenceRequest));
+        // Verify behavior
+        verify(validator).validateEvidenceRegistration(evidenceRequest);
+        verifyNoInteractions(workflowService, producer);
+    }
     @Test
     public void testUpdateEvidence() {
         // Prepare data
@@ -73,5 +108,38 @@ public class EvidenceServiceTest {
         verify(validator).validateApplicationExistence(evidenceRequest);
         verify(workflowService).updateWorkflowStatus(evidenceRequest);
         verify(producer).push("update_topic", evidenceRequest);
+    }
+    @Test
+    public void testUpdateEvidenceWithValidationException() {
+        // Prepare data
+        EvidenceRequest evidenceRequest = new EvidenceRequest();
+        evidenceRequest.setArtifact(new Artifact());
+
+        // Mock behavior
+        when(validator.validateApplicationExistence(evidenceRequest)).thenThrow(new RuntimeException("Validation Exception"));
+
+        // Execute and assert
+        assertThrows(CustomException.class, () -> evidenceService.updateEvidence(evidenceRequest));
+
+        // Verify behavior
+        verify(validator).validateApplicationExistence(evidenceRequest);
+        verifyNoInteractions(workflowService, producer);
+    }
+
+    @Test
+    public void testUpdateEvidenceWithOtherException() {
+        // Prepare data
+        EvidenceRequest evidenceRequest = new EvidenceRequest();
+        evidenceRequest.setArtifact(new Artifact());
+
+        // Mock behavior
+        when(validator.validateApplicationExistence(evidenceRequest)).thenThrow(new NullPointerException("Null Pointer Exception"));
+
+        // Execute and assert
+        assertThrows(CustomException.class, () -> evidenceService.updateEvidence(evidenceRequest));
+
+        // Verify behavior
+        verify(validator).validateApplicationExistence(evidenceRequest);
+        verifyNoInteractions(workflowService, producer);
     }
 }
