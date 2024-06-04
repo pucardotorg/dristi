b9bef189e2b284bab93a120a58a0e455d9ef1fd6
atulgupta2024 <161275709+atulgupta2024@users.noreply.github.com>
2024-06-04 14:17:10 +0530

    Update backend/application/src/main/java/org/pucar/dristi/repository/rowMapper/StatuteSectionRowMapper.java
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- backend/application/src/main/java/org/pucar/dristi/repository/rowMapper/StatuteSectionRowMapper.java
+++ backend/application/src/main/java/org/pucar/dristi/repository/rowMapper/StatuteSectionRowMapper.java
@@ -64,7 +64,7 @@ public class StatuteSectionRowMapper implements ResultSetExtractor<Map<UUID, Sta
     public List<String> stringToList(String str){
         List<String> list = new ArrayList<>();
         if(str!=null){
-            StringTokenizer st = new StringTokenizer("str","|");
+            StringTokenizer st = new StringTokenizer(str,"|");
             while (st.hasMoreTokens()) {
                 list.add(st.nextToken());
             }
