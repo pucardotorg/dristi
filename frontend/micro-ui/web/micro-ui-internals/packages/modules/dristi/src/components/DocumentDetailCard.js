f1e8810ece70f965a19208e7bd239a3407d2f08e
shambhavigoel1002 <shambhavi.g@beehyv.com>
2024-06-04 17:18:28 +0530

    application details screen changes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/DocumentDetailCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/DocumentDetailCard.js
@@ -1,22 +1,26 @@
-import { Header } from "@egovernments/digit-ui-react-components";
+import { Card, Header } from "@egovernments/digit-ui-react-components";
 import React from "react";
 
 function DocumentDetailCard({ cardData, header }) {
   return (
-    <div className="notice_and_circular_main gap-ten" style={{ flex: 1, margin: "5px 20px" }}>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+    <Card style={{ flex: 1, margin: "5px 20px", border: "1px solid #D9D9D9", boxShadow: "none" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
         {header && <Header styles={{ fontSize: "24px" }}>{header}</Header>}
         {cardData.map((row) => (
           <div>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <p className="documentDetails_title">{row?.title}</p> <p>{row?.content}</p>
-            </div>
+            {row?.title || row?.content ? (
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p className="documentDetails_title">{row?.title}</p>
+                <p>{row?.content}</p>
+              </div>
+            ) : null}
+            <div>{row?.doc}</div>
             {row?.image?.content}
             {row?.icon}
           </div>
         ))}
       </div>
-    </div>
+    </Card>
   );
 }
 
