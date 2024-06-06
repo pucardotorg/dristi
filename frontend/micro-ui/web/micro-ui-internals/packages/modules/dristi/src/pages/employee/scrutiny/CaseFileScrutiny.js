af21798618d3f32e4d2bffde2a19beead5037486
vaibhavc <vaibhav.c@beehyv.com>
2024-06-06 19:03:28 +0530

    Scrutiny Error Count

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/scrutiny/CaseFileScrutiny.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/scrutiny/CaseFileScrutiny.js
@@ -1,5 +1,5 @@
 import { FormComposerV2, Header, Toast } from "@egovernments/digit-ui-react-components";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { CustomArrowDownIcon } from "../../../icons/svgIndex";
 import { reviewCaseFileFormConfig } from "../../citizen/FileCase/Config/reviewcasefileconfig";
 
@@ -7,6 +7,48 @@ function CaseFileScrutiny({ t }) {
   const [isDisabled, setIsDisabled] = useState(false);
   const [showErrorToast, setShowErrorToast] = useState(false);
   const [formdata, setFormdata] = useState({ isenabled: true, data: {}, displayindex: 0 });
+
+  const countSectionErrors = (section) => {
+    let count = 0;
+
+    if (section?.scrutinyMessage && section?.scrutinyMessage?.FSOError) {
+      count++;
+    }
+
+    Object.keys(section)?.forEach((key) => {
+      if (section[key]) {
+        debugger;
+        if (section[key]?.scrutinyMessage) {
+          count++;
+        }
+        section[key]?.form?.forEach((item) => {
+          Object.keys(item)?.forEach((field) => {
+            if (item[field]?.FSOError) {
+              count++;
+            }
+          });
+        });
+      }
+    });
+
+    return count;
+  };
+
+  const scrutinyErrorCount = useMemo(() => {
+    const errorCount = {};
+    for (const key in formdata?.data) {
+      if (typeof formdata.data[key] === "object" && formdata.data[key] !== null) {
+        if (!errorCount[key]) {
+          errorCount[key] = 0;
+        }
+        errorCount[key] += countSectionErrors(formdata.data[key]);
+      }
+    }
+    return errorCount;
+  }, [formdata]);
+
+  //error count for each section
+  console.debug(scrutinyErrorCount);
   const onSubmit = () => {};
   const onSaveDraft = () => {};
   const onFormValueChange = (setValue, formData, formState, reset, setError, clearErrors, trigger, getValues) => {
@@ -24,9 +66,9 @@ function CaseFileScrutiny({ t }) {
     <div className="file-case">
       <div className="file-case-side-stepper">
         <div className="file-case-select-form-section">
-          <div className="accordion-wrapper">Litigent Details</div>
-          <div className="accordion-wrapper">Case Specific Details</div>
-          <div className="accordion-wrapper">Additional Details</div>
+          <div className="accordion-wrapper">{`Litigent Details ${}`}</div>
+          <div className="accordion-wrapper">{`Case Specific Details`}</div>
+          <div className="accordion-wrapper">{`Additional Details`}</div>
         </div>
       </div>
       <div className="file-case-form-section">
