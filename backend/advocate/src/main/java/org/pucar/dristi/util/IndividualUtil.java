bbf9740a9983b6bfe0f6b6125343d4ff0f4b6d22
babstar93 <140029411+babstar93@users.noreply.github.com>
2024-06-10 13:24:08 +0000

    Fix issue when searching case, add missing files for advocate project (#388)
    Co-authored-by: Babacar Ndiaye <babacar.ndiay93@gmail.com>

---
+++ backend/advocate/src/main/java/org/pucar/dristi/util/IndividualUtil.java
@@ -0,0 +1,65 @@
+package org.pucar.dristi.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.*;
+import lombok.extern.slf4j.Slf4j;
+import org.egov.tracer.model.CustomException;
+import org.pucar.dristi.repository.ServiceRequestRepository;
+import org.pucar.dristi.web.models.IndividualSearchRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static org.pucar.dristi.config.ServiceConstants.INDIVIDUAL_UTILITY_EXCEPTION;
+
+@Component
+@Slf4j
+public class IndividualUtil {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private ServiceRequestRepository serviceRequestRepository;
+
+
+    @Autowired
+    public IndividualUtil(ObjectMapper mapper, ServiceRequestRepository serviceRequestRepository) {
+        this.mapper = mapper;
+        this.serviceRequestRepository = serviceRequestRepository;
+    }
+
+    /** To call the individual service for searching individualId exists or not
+     * @param individualRequest
+     * @param uri
+     * @param individualUserUUID
+     * @return
+     */
+    public Boolean individualCall(IndividualSearchRequest individualRequest, StringBuilder uri, Map<String, String> individualUserUUID) {
+        try{
+            Object responseMap = serviceRequestRepository.fetchResult(uri, individualRequest);
+            if(responseMap!=null){
+                Gson gson= new Gson();
+                String jsonString=gson.toJson(responseMap);
+                log.info("Response :: {}", jsonString);
+                JsonObject response = JsonParser.parseString(jsonString).getAsJsonObject();
+                JsonArray individualObject=response.getAsJsonArray("Individual");
+                if(!individualObject.isEmpty()) {
+                    String userUUID = individualObject.get(0).getAsJsonObject().get("userUuid").getAsString();
+                    individualUserUUID.put("userUuid", userUUID);
+                }
+                return !individualObject.isEmpty() && individualObject.get(0).getAsJsonObject().get("individualId") != null;
+            }
+            return false;
+        }
+        catch (CustomException e) {
+            log.error("Custom Exception occurred in Individual Utility :: {}", e.toString());
+            throw e;
+        }
+        catch (Exception e){
+            throw new CustomException(INDIVIDUAL_UTILITY_EXCEPTION,"Error in individual utility service: "+e.getMessage());
+        }
+
+    }
+}
