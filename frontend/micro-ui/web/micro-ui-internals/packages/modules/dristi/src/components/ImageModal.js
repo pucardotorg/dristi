e555a093d4dca11db44bb499f9e5da7eaacd702a
Nitish Kumar <nitish.k@beehyv.com>
2024-06-13 21:37:43 +0530

    image modal ui fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/ImageModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/ImageModal.js
@@ -1,29 +1,39 @@
 import React from "react";
 import Modal from "./Modal";
-import { FlagIcon } from "../icons/svgIndex";
+import { FlagIcon, LeftArrow } from "../icons/svgIndex";
 import { CloseSvg } from "@egovernments/digit-ui-react-components";
 import DocViewerWrapper from "../pages/employee/docViewerWrapper";
 
 function ImageModal({ imageInfo, handleCloseModal, handleOpenPopup, t }) {
   const tenantId = window?.Digit.ULBService.getCurrentTenantId();
   const Heading = (props) => {
-    return <h1 className="heading-m">{props.label}</h1>;
+    return (
+      <div className="heading-main">
+        <div className="heading-back-error">
+          <LeftArrow />
+        </div>
+        <div className="heading-title">
+          <h1 className="heading-m">{props.label}</h1>
+          <p>{`cheque_bounced.jpeg  |  2MB`}</p>
+        </div>
+      </div>
+    )
+
   };
   const HeaderBarEnd = () => {
     return (
-      <div style={{ display: "flex", gap: 20, alignItems: "center", justifyContent: "end" }}>
-        <div
+      <React.Fragment>
+        <div className="flag-icon"
           onClick={(e) => {
             handleOpenPopup(e, imageInfo.configKey, imageInfo.name, imageInfo.dataIndex, imageInfo.fieldName);
           }}
-          style={{ height: "100%", display: "flex", alignItems: "center", paddingRight: "20px", cursor: "pointer" }}
         >
           <FlagIcon />
         </div>
-        <div onClick={handleCloseModal} style={{ height: "100%", display: "flex", alignItems: "center", paddingRight: "20px", cursor: "pointer" }}>
+        <div className="close-icon">
           <CloseSvg />
         </div>
-      </div>
+      </React.Fragment>
     );
   };
   return (
