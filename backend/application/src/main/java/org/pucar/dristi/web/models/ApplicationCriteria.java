691f88828de27b1c1d965bb61e616d6562aa55b9
Nilesh Gupta <nilesh.gupta@beehyv.com>
2024-06-26 11:19:19 +0530

    updated tests

--- backend/application/src/main/java/org/pucar/dristi/web/models/ApplicationCriteria.java
+++ backend/application/src/main/java/org/pucar/dristi/web/models/ApplicationCriteria.java
@@ -8,7 +8,8 @@ import jakarta.validation.Valid;
 
 @Validated
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-15T11:31:40.281899+05:30[Asia/Kolkata]")
-@Data
+@Getter
+@Setter
 public class ApplicationCriteria {
 
     @JsonProperty("id")
--- backend/application/src/main/java/org/pucar/dristi/web/models/ApplicationSearchRequest.java
+++ backend/application/src/main/java/org/pucar/dristi/web/models/ApplicationSearchRequest.java
@@ -1,22 +1,20 @@
 package org.pucar.dristi.web.models;
 
+import lombok.*;
 import org.egov.common.contract.request.RequestInfo;
 import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * CaseSearchRequest
  */
 @Validated
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-15T11:31:40.281899+05:30[Asia/Kolkata]")
-@Data
+@Getter
+@Setter
 public class ApplicationSearchRequest {
 
     @JsonProperty("RequestInfo")
