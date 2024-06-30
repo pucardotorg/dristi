5d3af5b893734e74320996201b533f80892ce8db
Teja <divyateja.konatam@beehyv.com>
2024-06-30 16:25:02 +0530

    removed console logs and added modal

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/CalendarView.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/CalendarView.js
@@ -4,10 +4,13 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import useGetHearings from "../hooks/dristi/useGetHearings";
+import { Modal } from "@egovernments/digit-ui-react-components";
+const tenantId = window.localStorage.getItem("tenant-id");
 
 const MonthlyCalendar = () => {
-  const tenantId = "pg";
-  const [currentMonth, setCurrentMonth] = useState(new Date()); // State to track the current month
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [showModal, setShowModal] = useState(false);
+  const [currentEvent, setCurrentEvent] = useState(null);
   const { data: hearingResponse, refetch: refetch } = useGetHearings(
     { hearing: { tenantId }, tenantId },
     { applicationNumber: "", cnrNumber: "", tenantId },
@@ -73,46 +76,70 @@ const MonthlyCalendar = () => {
 
   const Calendar_events = useMemo(() => {
     const calendarEvents = {};
-  
+
     hearingDetails.forEach((hearing) => {
       const dateTimeObj = epochToDateTimeObject(hearing.startTime);
-  
+
       if (dateTimeObj) {
-        const dateString = dateTimeObj.date; 
+        const dateString = dateTimeObj.date;
         events.slots.forEach((slot) => {
-          if (dateTimeObj.time>= slot.slotStartTime && dateTimeObj.time< slot.slotEndTime) {
+          if (dateTimeObj.time >= slot.slotStartTime && dateTimeObj.time < slot.slotEndTime) {
             const eventKey = `${dateString}-${slot.slotName}`;
-  
+
             if (!calendarEvents[eventKey]) {
               calendarEvents[eventKey] = {
                 title: `${slot.slotName} Hearing`,
                 start: `${dateString}T${slot.slotStartTime}`,
-                end: `${dateString}T${slot.slotEndTime}`, 
+                end: `${dateString}T${slot.slotEndTime}`,
                 extendedProps: {
                   hearings: [hearing],
-                  count:1,
+                  count: 1,
                   date: new Date(dateString),
                   slot: slot.slotName,
                 },
               };
             } else {
-              calendarEvents[eventKey].extendedProps.count  += 1;
+              calendarEvents[eventKey].extendedProps.count += 1;
               calendarEvents[eventKey].extendedProps.hearings.push(hearing);
             }
           }
         });
       }
     });
-  
+
     const eventsArray = Object.values(calendarEvents);
-  
-    console.log('calendarEvents', eventsArray);
     return eventsArray;
   }, [hearingDetails, events.slots]);
-  
-  
-  
-  
+
+  const handleEventClick = (arg) => {
+    setCurrentEvent(arg.event);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setCurrentEvent(null);
+  };
+
+  const Close = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g>
+        <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#0A0A0A" />
+      </g>
+    </svg>
+  );
+
+  const CloseBtn = (props) => {
+    return (
+      <div style={{ padding: "10px" }} onClick={props.onClick}>
+        <Close />
+      </div>
+    );
+  };
+
+  const Heading = (props) => {
+    return <h1 className="heading-m">{props.label}</h1>;
+  };
 
   return (
     <div>
@@ -127,10 +154,21 @@ const MonthlyCalendar = () => {
         height={"85vh"}
         events={Calendar_events}
         eventContent={(arg) => {
-          console.log('argEventStart',arg);
           return <div>{`${arg.event.extendedProps.slot} : ${arg.event.extendedProps.count}-hearings`}</div>;
         }}
+        eventClick={handleEventClick}
       />
+
+      {showModal && (
+        <Modal headerBarMain={<Heading label={"hearings"} />} headerBarEnd={<CloseBtn onClick={() => setShowModal(false)} />} onClose={closeModal}>
+          {currentEvent.extendedProps.hearings.map((hearing) => (
+            <div>
+              <div key={hearing.id}>id :- {hearing.id}</div>
+              <div> name:- {hearing.additionalDetails.name}</div>
+            </div>
+          ))}
+        </Modal>
+      )}
     </div>
   );
 };
