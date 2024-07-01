e28f0ef3276250a916bcb77cf7cd2561bcf659c5
vaibhavc <vaibhav.c@beehyv.com>
2024-07-01 13:09:13 +0530

    added few configs

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/submissions/src/pages/employee/SubmissionsCreate.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/submissions/src/pages/employee/SubmissionsCreate.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import { FormComposerV2, Header } from "@egovernments/digit-ui-react-components";
 import { configs } from "../../configs/submissionsCreateConfig";
 import { transformCreateData } from "../../utils/createUtils";
 
-const fieldStyle={ marginRight: 0 };
+const fieldStyle = { marginRight: 0 };
 
 const SubmissionsCreate = () => {
-  const defaultValue={};
+  const defaultValue = {};
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const { t } = useTranslation();
   const history = useHistory();
+  const [formdata, setFormdata] = useState({});
   const reqCreate = {
     url: `/application/application/v1/create`,
     params: {},
@@ -20,6 +21,7 @@ const SubmissionsCreate = () => {
       enable: false,
     },
   };
+  const modifiedFormConfig = useMemo(() => {}, []);
 
   const mutation = Digit.Hooks.useCustomAPIMutationHook(reqCreate);
   const onError = (resp) => {
@@ -28,12 +30,19 @@ const SubmissionsCreate = () => {
 
   const onSuccess = (resp) => {
     history.push(`/${window.contextPath}/employee/submissions/submissions-response?appNo=${"NEW-NO-1"}&isSuccess=${true}`, {
-      message:  "SUBMISSION_CREATION_SUCCESS",
+      message: "SUBMISSION_CREATION_SUCCESS",
       showID: true,
       label: "SUBMISSION_ID",
     });
   };
-  const onSubmit = async(data) => {
+
+  const onFormValueChange = (setValue, formData, formState, reset, setError, clearErrors, trigger, getValues) => {
+    if (JSON.stringify(formData) !== JSON.stringify(formdata)) {
+      setFormdata(formData);
+    }
+  };
+
+  const onSubmit = async (data) => {
     console.log(data, "data");
     await mutation.mutate(
       {
@@ -49,12 +58,12 @@ const SubmissionsCreate = () => {
         onError,
       }
     );
-   
   };
   return (
     <div>
       <Header> {t("CREATE_SUBMISSION")}</Header>
-       <FormComposerV2
+
+      <FormComposerV2
         label={t("SUBMIT_BUTTON")}
         config={configs.map((config) => {
           return {
@@ -62,15 +71,12 @@ const SubmissionsCreate = () => {
           };
         })}
         defaultValues={defaultValue}
-        onFormValueChange ={ (setValue, formData, formState, reset, setError, clearErrors, trigger, getValues) => {
-          console.log(formData, "formData");
-        }}
-        onSubmit={(data,) => onSubmit(data, )}
+        onFormValueChange={onFormValueChange}
+        onSubmit={(data) => onSubmit(data)}
         fieldStyle={fieldStyle}
-      /> 
-       
+      />
     </div>
   );
-}
+};
 
 export default SubmissionsCreate;
