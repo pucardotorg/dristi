ef265bb6ce5d12c29672ca567debc41423bdba2d
beehyvpilot1 <beehyvpilot1@beehyv.com>
2024-07-01 16:01:18 +0530

    updated with sonarqube feedback comments

--- backend/task/src/test/java/org/pucar/dristi/config/MainConfigurationTest.java
+++ backend/task/src/test/java/org/pucar/dristi/config/MainConfigurationTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-public class MainConfigurationTest {
+class MainConfigurationTest {
 
     @Mock
     private ObjectMapper objectMapper;
