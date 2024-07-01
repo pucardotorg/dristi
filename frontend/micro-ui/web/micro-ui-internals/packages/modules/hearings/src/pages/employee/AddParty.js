ee14465d91f8ea0f8f2ce4ea23decc0125ad2dbd
roopesh.sagar <roopesh.sagar@beehyv.com>
2024-07-01 10:52:02 +0530

    changes for add party component

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/AddParty.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/AddParty.js
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useState , useMemo} from "react";
+import { FormComposer, Modal, Button, FormComposerV2 } from "@egovernments/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router-dom";
+import addNewPartiesConfig from "../../configs/AddNewPartyConfig";
 
+const AddParty = ({ onClose }) => {
+  const { t } = useTranslation();
+  const [partyFields, setPartyFields] = useState([
+    { partyType: "Witness", partyName: "", phoneNumber: "", emailId: "", address: "", additionalDetails: "" },
+  ]);
 
-const fieldStyle={ marginRight: 0 };
+  const handleAddParty = () => {
+    setPartyFields([...partyFields, { partyType: "Witness", partyName: "", phoneNumber: "", emailId: "", address: "", additionalDetails: "" }]);
+  };
 
-const AddParty = () => {
+  const handleSubmit = (data) => {
+    console.log("Form data:", data);
+  };
 
+  const config = addNewPartiesConfig();
+  console.log(config,"config");
+
+  const simpleConfig = {
+    label: "Simple Form",
+    form: [
+      {
+        head: "Section 1",
+        body: [
+          {
+            label: "Field 1",
+            type: "text",
+            key: "field1",
+            isMandatory: true,
+            populators: {
+              name: "field1",
+            },
+          },
+        ],
+      },
+    ],
+  };
+  console.log(simpleConfig,"simpleconfig")
 
   return (
-    <div>
-        in add party page
-    </div>
+    <Modal
+      headerBarMain={<h1>{t("Add New Parties")}</h1>}
+      headerBarEnd={<Button onClick={onClose} label="Close" />}
+      actionCancelLabel="Back"
+      actionCancelOnSubmit={onClose}
+      actionSaveLabel="Add"
+      actionSaveOnSubmit={handleSubmit}
+    >
+      <FormComposerV2
+        config={simpleConfig}
+        onSubmit={handleSubmit}
+        inlineChildren
+      ></FormComposerV2>
+    </Modal>
   );
-}
+};
 
 export default AddParty;
