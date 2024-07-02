9851bbbb45da2be157b4ef2faef54c6381f848f7
ShivamAggarwal4 <115536482+ShivamAggarwal4@users.noreply.github.com>
2024-07-02 17:11:17 +0530

    ACCESSCODE_LENGTH change (#714)

--- backend/case/src/main/java/org/pucar/dristi/config/ServiceConstants.java
+++ backend/case/src/main/java/org/pucar/dristi/config/ServiceConstants.java
@@ -92,7 +92,7 @@ public class ServiceConstants {
 	public static final String TAX_HEADMASTER_CODE = "CASE_ADVANCE_CARRYFORWARD";
 	public static final String CREATE_DEMAND_STATUS = "PAYMENT_PENDING";
 	public static final String CASE_ADMIT_STATUS = "CASE_ADMITTED";
-	public static final int ACCESSCODE_LENGTH = 8;
+	public static final int ACCESSCODE_LENGTH = 6;
 	public static final String STATE = "KL";
 	public static final String DISTRICT = "JL";
 	public static final String ESTABLISHMENT_CODE = "01";
