58f35ca566962e93c652161f4ddb3731a4677fc7
storm20198 <rajat.g@beehyv.com>
2024-07-02 12:27:11 +0530

    fixes

--- backend/analytics/src/main/java/org/pucar/dristi/config/ServiceConstants.java
+++ backend/analytics/src/main/java/org/pucar/dristi/config/ServiceConstants.java
@@ -13,7 +13,7 @@ public class ServiceConstants {
 	//Consumer
 	public static final String ES_INDEX_HEADER_FORMAT = "{\"index\":{\"_index\":\"%s\",\"_id\":\"%s\"}}\n";
 	public static final String ES_INDEX_DOCUMENT_FORMAT = "{\"Data\": {\"id\":\"%s\",\"entityType\":\"%s\",\"referenceId\":\"%s\",\"status\":\"%s\",\"assignedTo\":%s,\"assignedRole\":%s,\"cnrNumber\":\"%s\",\"filingNumber\":\"%s\",\"isCompleted\":%b,\"stateSla\":%d,\"businessServiceSla\":%d}}\n";
-	public static final String ES_INDEX_CASE_OVERALL_STATUS_DOCUMENT_FORMAT = "{\"Data\": {\"id\":\"%s\",\"entityType\":\"%s\",\"referenceId\":\"%s\",\"status\":\"%s\",\"tenantId\":%s,\"action\":%s,\"isCompleted\":\"%s\",\"stage\":\"%s\",\"subStage\"%s}}\n";
+	public static final String ES_INDEX_CASE_OVERALL_STATUS_DOCUMENT_FORMAT = "{\"Data\": {\"id\":\"%s\",\"entityType\":\"%s\",\"referenceId\":\"%s\",\"status\":\"%s\",\"tenantId\":\"%s\",\"action\":\"%s\",\"isCompleted\":\"%s\",\"stage\":\"%s\",\"subStage\":\"%s\"}}\n";
 	public static final String TENANTID_MDC_STRING = "TENANTID";
 
 	//Search Param
