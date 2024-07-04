842d95a2dd558f2de6a4d31fea56dedb87d9cc71
nilesh <nilesh.gupta@beehyv.com>
2024-07-04 17:16:41 +0530

    updated test covergae

--- backend/order/src/main/java/org/pucar/dristi/web/models/IssuedBy.java
+++ backend/order/src/main/java/org/pucar/dristi/web/models/IssuedBy.java
@@ -2,10 +2,7 @@ package org.pucar.dristi.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
@@ -14,10 +11,8 @@ import java.util.UUID;
 @Schema(description = "details of issuer of order")
 @Validated
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-18T11:14:11.072458+05:30[Asia/Calcutta]")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Getter
+@Setter
 public class IssuedBy {
 
     @JsonProperty("benchID")
