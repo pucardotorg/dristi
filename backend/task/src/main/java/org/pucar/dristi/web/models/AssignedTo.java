ac1dfa13149a616ae0251da399d372a95a47a754
nilesh <nilesh.gupta@beehyv.com>
2024-07-04 16:36:20 +0530

    updated description

--- backend/task/src/main/java/org/pucar/dristi/web/models/AssignedTo.java
+++ backend/task/src/main/java/org/pucar/dristi/web/models/AssignedTo.java
@@ -7,10 +7,7 @@ import org.springframework.validation.annotation.Validated;
 
 import java.util.UUID;
 
-/**
- * a case can have multiple hearings. this represents one of the many hearings related to the case
- */
-@Schema(description = "A hearing is mostly presided by a Judge, but there is discussion on some hearing being presided by an Admin person. Hence this field will store the ID of the specific person")
+@Schema(description = "details of person this task is assigned to. For example in case of document upload, this could be litigant or lawyer. in case of summon task, this will be person to whom summon is issued")
 @Validated
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-18T11:14:11.072458+05:30[Asia/Calcutta]")
 @Getter
