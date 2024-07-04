1139e27c06bfda5b1c67bd245c1fcd7e66441167
ishaandamadia <ishaan.d@beehyv.com>
2024-07-04 13:40:34 +0530

    using builder instead of setters, removed @Valid

--- backend/hearing/src/main/java/org/pucar/dristi/web/models/HearingCriteria.java
+++ backend/hearing/src/main/java/org/pucar/dristi/web/models/HearingCriteria.java
@@ -1,7 +1,7 @@
 package org.pucar.dristi.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Valid;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
@@ -12,46 +12,37 @@ import java.time.LocalDate;
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-15T11:31:40.281899+05:30[Asia/Kolkata]")
 @Getter
 @Setter
+@Builder
 public class HearingCriteria {
 
     @JsonProperty("hearingId")
-    @Valid
     private String hearingId = null;
 
     @JsonProperty("cnrNumber")
-    @Valid
     private String cnrNumber = null;
 
     @JsonProperty("filingNumber")
-    @Valid
     private String filingNumber = null;
 
     @JsonProperty("tenantId")
-    @Valid
     private String tenantId = null;
 
     @JsonProperty("applicationNumber")
-    @Valid
     private String applicationNumber = null;
 
     @JsonProperty("fromDate")
-    @Valid
     private LocalDate fromDate = null;
 
     @JsonProperty("toDate")
-    @Valid
     private LocalDate toDate = null;
 
     @JsonProperty("limit")
-    @Valid
     private Integer limit = null;
 
     @JsonProperty("offset")
-    @Valid
     private Integer offset = null;
 
     @JsonProperty("sortBy")
-    @Valid
     private String sortBy = null;
 
 }
