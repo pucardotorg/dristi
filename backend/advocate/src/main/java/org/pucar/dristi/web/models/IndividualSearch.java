0e41ca27a13a17d24eead3aa368752c95ffcc8ad
ShivamAggarwal4 <shivam.a@beehyv.com>
2024-07-09 17:23:30 +0530

    COMMENTS FIX

--- backend/advocate/src/main/java/org/pucar/dristi/web/models/IndividualSearch.java
+++ backend/advocate/src/main/java/org/pucar/dristi/web/models/IndividualSearch.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.egov.common.models.individual.Gender;
 import org.egov.common.models.individual.Identifier;
 import org.egov.common.models.individual.Name;
 import org.springframework.validation.annotation.Validated;
