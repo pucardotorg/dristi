11cbc66ee6125348a7e18bd04c14138ebdef3459
Suresh Soren <sureshsoren@beehyv.com>
2024-07-16 17:19:35 +0530

    latitude longitude fix for efiling

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/CaseType.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/CaseType.js
@@ -261,8 +261,8 @@ function CaseType({ t }) {
                                       city: city,
                                       state: addressLine1,
                                       coordinates: {
-                                        longitude: latitude,
-                                        latitude: longitude,
+                                        longitude: longitude,
+                                        latitude: latitude,
                                       },
                                       locality: address,
                                     },
