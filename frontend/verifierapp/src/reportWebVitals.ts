c4ee7387d8fc24a0710cc379c7beb00d7fda38ff
Tushar S Phalke <44227068+tusharphalke19995@users.noreply.github.com>
2024-07-18 18:39:04 +0530

    creating verifier repo in fronend folder which in pucar master repo (#896)
    Co-authored-by: satishkumar078 <37110021+satishkumar078@users.noreply.github.com>

---
+++ frontend/verifierapp/src/reportWebVitals.ts
@@ -0,0 +1,17 @@
+import { ReportHandler } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    }).catch((error) => {
+      console.error("reportWebVitals error", error);
+    });
+  }
+};
+
+export default reportWebVitals;
