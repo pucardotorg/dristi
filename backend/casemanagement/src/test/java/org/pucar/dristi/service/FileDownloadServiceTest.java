c452c1cd5d22e20fc27b7615dd4f02f6d94cb3d0
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-07-22 15:37:51 +0530

    udpated file download test case

--- backend/casemanagement/src/test/java/org/pucar/dristi/service/FileDownloadServiceTest.java
+++ backend/casemanagement/src/test/java/org/pucar/dristi/service/FileDownloadServiceTest.java
@@ -1,34 +1,16 @@
 package org.pucar.dristi.service;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.egov.common.contract.request.RequestInfo;
 import org.egov.tracer.model.CustomException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.pucar.dristi.config.Configuration;
-import org.pucar.dristi.web.models.VcCredentialRequest;
-import org.springframework.util.StreamUtils;
-
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FileDownloadServiceTest {
-
-    @Mock
-    private Configuration configuration;
 
     @InjectMocks
     @Spy
