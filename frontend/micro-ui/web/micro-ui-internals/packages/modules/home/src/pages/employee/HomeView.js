02e4bc95f5b46a49b60aef91e645c6f41ca68d43
Teja <divyateja.konatam@beehyv.com>
2024-07-25 14:36:20 +0530

    added the redirect to summons and warrants

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/HomeView.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/HomeView.js
@@ -184,9 +184,22 @@ const HomeView = () => {
     setConfig(tabConfig?.TabSearchConfig?.[n]);
   };
 
-  const handleNavigate = () => {
+  const handleNavigate = (route) => {
     const contextPath = window?.contextPath || "";
-    history.push(`/${contextPath}/${userInfoType}/hearings/view-hearing`);
+    history.push(`/${contextPath}/${userInfoType}/${route}`);
+  };
+
+  const BtnToRedirect = (props) => {
+    return (
+      <button onClick={props?.onClick}>
+        <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M15.3172 7.44254L9.69219 13.0675C9.57491 13.1848 9.41585 13.2507 9.25 13.2507C9.08415 13.2507 8.92509 13.1848 8.80781 13.0675C8.69054 12.9503 8.62465 12.7912 8.62465 12.6253C8.62465 12.4595 8.69054 12.3004 8.80781 12.1832L13.3664 7.62535H1.125C0.95924 7.62535 0.800269 7.5595 0.683058 7.44229C0.565848 7.32508 0.5 7.16611 0.5 7.00035C0.5 6.83459 0.565848 6.67562 0.683058 6.55841C0.800269 6.4412 0.95924 6.37535 1.125 6.37535H13.3664L8.80781 1.81753C8.69054 1.70026 8.62465 1.5412 8.62465 1.37535C8.62465 1.2095 8.69054 1.05044 8.80781 0.93316C8.92509 0.815885 9.08415 0.75 9.25 0.75C9.41585 0.75 9.57491 0.815885 9.69219 0.93316L15.3172 6.55816C15.3753 6.61621 15.4214 6.68514 15.4528 6.76101C15.4843 6.83688 15.5005 6.91821 15.5005 7.00035C15.5005 7.08248 15.4843 7.16381 15.4528 7.23969C15.4214 7.31556 15.3753 7.38449 15.3172 7.44254Z"
+            fill="#0E7669"
+          />
+        </svg>
+      </button>
+    );
   };
   const JoinCaseHome = Digit?.ComponentRegistryService?.getComponent("JoinCaseHome");
 
@@ -245,7 +258,35 @@ const HomeView = () => {
       ) : (
         <React.Fragment>
           <div className="left-side">
-            <UpcomingHearings handleNavigate={handleNavigate} />
+            <UpcomingHearings handleNavigate={() => handleNavigate("hearings/view-hearing")} />
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                backgroundColor: "#f7f5f3",
+                borderRadius: "4px",
+                padding: "16px",
+                marginTop: "16px",
+              }}
+            >
+              <div>Review, Summons & Warrants</div>
+              <BtnToRedirect onClick={() => handleNavigate("orders/Summons&Notice")} />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                backgroundColor: "#f7f5f3",
+                borderRadius: "4px",
+                padding: "16px",
+                marginTop: "4px",
+              }}
+            >
+              <div>View Issued Orders</div>
+              <BtnToRedirect />
+            </div>
             <div className="content-wrapper">
               <div className="header-class">
                 <div className="header">{t("CS_YOUR_CASE")}</div>
