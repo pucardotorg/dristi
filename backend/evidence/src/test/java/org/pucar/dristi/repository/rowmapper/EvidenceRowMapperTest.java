6b6b6226bda2fa186f40b16759a8ed13ab273537
ShivamAggarwal4 <shivam.a@beehyv.com>
2024-07-26 17:49:47 +0530

    evidence codesmell fix

--- backend/evidence/src/test/java/org/pucar/dristi/repository/rowmapper/EvidenceRowMapperTest.java
+++ backend/evidence/src/test/java/org/pucar/dristi/repository/rowmapper/EvidenceRowMapperTest.java
@@ -1,7 +1,6 @@
 package org.pucar.dristi.repository.rowmapper;
 
 import org.egov.common.contract.models.Document;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
