e380e824923adb653a32fa03fcefc9c7d83c9efe
Suresh Soren <sureshsoren@beehyv.com>
2024-07-30 17:43:17 +0530

    home count fix and advocate uuid efiling mapping fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/components/TaskComponent.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/components/TaskComponent.js
@@ -333,7 +333,7 @@ const TasksComponent = ({ taskType, setTaskType, isLitigant, uuid, filingNumber 
       <h2>{!isLitigant ? "Your Tasks" : t("ALL_PENDING_TASK_TEXT")}</h2>
       <div className="task-filters">
         <LabelFieldPair>
-          <CardLabel className={"card-label"}>{`Case Type`}</CardLabel>
+          <CardLabel style={{ fontSize: "16px" }} className={"card-label"}>{`Case Type`}</CardLabel>
           <Dropdown
             option={[{ name: "NIA S138", code: "NIA S138" }]}
             selected={{ name: "NIA S138", code: "NIA S138" }}
@@ -343,7 +343,7 @@ const TasksComponent = ({ taskType, setTaskType, isLitigant, uuid, filingNumber 
           />
         </LabelFieldPair>
         <LabelFieldPair>
-          <CardLabel className={"card-label"}>{`Task Type`}</CardLabel>
+          <CardLabel style={{ fontSize: "16px" }} className={"card-label"}>{`Task Type`}</CardLabel>
           <Dropdown
             option={taskTypes}
             optionKey={"name"}
