a188e6f0992bea89d8dc77a42c595a81ed9e077a
nitish-beehyv <nitish.k@beehyv.com>
2024-08-01 16:00:45 +0530

    formate date and fixed no hearing box (#1142)
    Co-authored-by: Suresh Soren <sureshsoren@beehyv.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/HomeView.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/HomeView.js
@@ -231,7 +231,7 @@ const HomeView = () => {
           );
         } else if (row?.original?.status === "ADMISSION_HEARING_SCHEDULED") {
           history.push(
-            `/${window?.contextPath}/${userInfoType}/dristi/admission?caseId=${row?.original?.id}&filingNumber=${row?.original?.filingNumber}`
+            `/${window?.contextPath}/${userInfoType}/dristi/home/view-case?caseId=${row?.original?.id}&filingNumber=${row?.original?.filingNumber}&tab=Complaints`
           );
         } else {
           history.push(
