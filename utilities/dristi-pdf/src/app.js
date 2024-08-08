6a1b1409898387316d381e38ca48fc46941be74a
Varun Reddy <155146627+varunreddy-egov@users.noreply.github.com>
2024-08-08 15:24:41 +0530

    Order PDF BFF service (#1225)
    Co-authored-by: atulgupta2024 <161275709+atulgupta2024@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: rkum113 <raushan.k@beehyv.com>
    Co-authored-by: Rkum113 <107095632+Rkum113@users.noreply.github.com>

---
+++ utilities/dristi-pdf/src/app.js
@@ -0,0 +1,46 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var config = require("./config");
+
+const order = require("./routes/order");
+
+// var {listenConsumer} = require("./consumer")
+
+var app = express();
+app.disable('x-powered-by');
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(config.app.contextPath + "/order", order);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// Commenting consumer listener becuase excel bill gen is not required. IFMS adapter will process the payment.
+// listenConsumer();
+
+module.exports = app;
