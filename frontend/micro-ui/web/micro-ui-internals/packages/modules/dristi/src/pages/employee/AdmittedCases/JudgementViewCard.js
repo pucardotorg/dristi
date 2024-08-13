fabbe3a7e4ba2ea0ccb94d31013621726c24e869
vaibhavct <111569522+vaibhavct@users.noreply.github.com>
2024-08-14 01:28:51 +0530

    showing judgement date in case overview (#1365)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/AdmittedCases/JudgementViewCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/AdmittedCases/JudgementViewCard.js
@@ -1,11 +1,27 @@
 import { Button, Card } from "@egovernments/digit-ui-react-components";
-import React from "react";
+import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { JudgementIcon } from "../../../icons/svgIndex";
 const JudgementViewCard = ({ caseData, width }) => {
   const { t } = useTranslation();
   const handleButtonClick = () => {};
-
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+  const { data: hearingsData } = Digit.Hooks.hearings.useGetHearings(
+    { criteria: { tenantId }, tenantId },
+    { applicationNumber: "", cnrNumber: "", tenantId },
+    "dristi",
+    true
+  );
+  const hearingsList = useMemo(() => hearingsData?.HearingList?.sort((a, b) => b.startTime - a.startTime), [hearingsData]);
+  const formatDate = (date, format) => {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    if (format === "DD-MM-YYYY") {
+      return `${day}-${month}-${year}`;
+    }
+    return `${year}-${month}-${day}`;
+  };
   return (
     <Card
       style={{
@@ -29,9 +45,28 @@ const JudgementViewCard = ({ caseData, width }) => {
       </div>
       <hr style={{ border: "1px solid #FFF6E880" }} />
       <div style={{ display: "flex", justifyContent: "space-between", padding: "10px" }}>
-        <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-          <div className="hearingCard">
-            <div className="hearingDate">{t(caseData?.case?.outcome)}</div>
+        <div style={{ display: "flex", gap: "10px", justifyContent: "start", flexDirection: "column" }}>
+          <div
+            style={{
+              fontWeight: 700,
+              fontSize: "24px",
+              lineHeight: "28.13px",
+              color: "#231F20",
+              marginTop: "5px",
+            }}
+          >
+            {t(caseData?.case?.outcome)}
+          </div>
+          <div
+            style={{
+              fontWeight: 400,
+              fontSize: "14px",
+              lineHeight: "16.41px",
+              color: "#3D3C3C",
+              marginTop: "5px",
+            }}
+          >
+            {formatDate(new Date(hearingsList?.[hearingsList?.length - 1]?.startTime), "DD-MM-YYYY")}
           </div>
         </div>
         <Button variation={"outlined"} onButtonClick={handleButtonClick} label={t("VIEW_JUDGEMENT_ORDER")} />
