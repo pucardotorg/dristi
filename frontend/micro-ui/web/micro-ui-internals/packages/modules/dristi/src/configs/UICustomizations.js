1613593fae773e985669039eb34ff9c5f1cead3a
Nitish Kumar <nitish.k@beehyv.com>
2024-08-20 16:34:19 +0530

    Click action on entire button #1285

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/configs/UICustomizations.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/configs/UICustomizations.js
@@ -303,14 +303,11 @@ export const UICustomizations = {
           );
         case "Action":
           return (
-            <span className="action-link">
-              <Link
-                to={`/digit-ui/employee/dristi/registration-requests/details?applicationNo=${applicationNumber}&individualId=${value}&type=${usertype}`}
-              >
-                {" "}
-                {t("Verify")}
-              </Link>
-            </span>
+            <Link
+              to={`/digit-ui/employee/dristi/registration-requests/details?applicationNo=${applicationNumber}&individualId=${value}&type=${usertype}`}
+            >
+              <span className="action-link"> {t("Verify")}</span>
+            </Link>
           );
         case "User Type":
           return usertype === "clerk" ? "Advocate Clerk" : "Advocate";
