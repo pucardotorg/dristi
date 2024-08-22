bb33e40234541c148836d25b05f5b51a0101acb7
Suresh Soren <sureshsoren@beehyv.com>
2024-08-22 17:24:11 +0530

    routes fix for employee users to not access citizen routes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/hooks/dristi/useSearchCaseService.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/hooks/dristi/useSearchCaseService.js
@@ -5,7 +5,10 @@ function useSearchCaseService(reqData, params, moduleCode, caseId, enabled) {
   const client = useQueryClient();
   const { isLoading, data, isFetching, refetch, error } = useQuery(
     `GET_CASE_DETAILS_${moduleCode}_${caseId}`,
-    () => DRISTIService.searchCaseService(reqData, params),
+    () =>
+      DRISTIService.searchCaseService(reqData, params)
+        .then((data) => data)
+        .catch(() => ({})),
     {
       cacheTime: 0,
       enabled: Boolean(enabled),
