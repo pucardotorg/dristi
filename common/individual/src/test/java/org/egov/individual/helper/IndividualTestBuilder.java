badd37c914e055fb805d019060d9319c881a459f
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2024-08-27 13:10:40 +0530

    updated tests (#1433)

--- common/individual/src/test/java/org/egov/individual/helper/IndividualTestBuilder.java
+++ common/individual/src/test/java/org/egov/individual/helper/IndividualTestBuilder.java
@@ -1,13 +1,13 @@
 package org.egov.individual.helper;
 
 import org.egov.common.helper.AuditDetailsTestBuilder;
-import org.egov.common.models.individual.Address;
 import org.egov.common.models.individual.AddressType;
 import org.egov.common.models.individual.Gender;
 import org.egov.common.models.individual.Identifier;
-import org.egov.common.models.individual.Individual;
 import org.egov.common.models.individual.Name;
 import org.egov.common.models.individual.Skill;
+import org.egov.individual.web.models.Address;
+import org.egov.individual.web.models.Individual;
 
 import java.util.Arrays;
 
