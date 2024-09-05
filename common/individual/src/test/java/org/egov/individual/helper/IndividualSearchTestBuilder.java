6ed323ee79218df536a4238f9efbd77b3c4c76a6
radheshjoshi1 <radhesh.joshi@beehyv.com>
2024-09-05 10:59:34 +0530

    name validation in individual service

--- common/individual/src/test/java/org/egov/individual/helper/IndividualSearchTestBuilder.java
+++ common/individual/src/test/java/org/egov/individual/helper/IndividualSearchTestBuilder.java
@@ -3,8 +3,8 @@ package org.egov.individual.helper;
 
 import org.egov.common.models.individual.Gender;
 import org.egov.common.models.individual.Identifier;
-import org.egov.common.models.individual.Name;
 import org.egov.individual.web.models.IndividualSearch;
+import org.egov.individual.web.models.Name;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
