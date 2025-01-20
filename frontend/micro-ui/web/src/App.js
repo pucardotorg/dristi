2cd9fed51de7fa2ba2020f41b01705f52bf0b18f
nitish-beehyv <nitish.k@beehyv.com>
2025-01-20 19:07:20 +0530

    remove css import (#1557)
    * completed the change for the ticket #2886 and refactor the code
    
    * remove console log and unused style
    
    * removed true check
    
    * remove style tag from evidence modal
    
    * remove css import

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -12,8 +12,6 @@ import { initHearingsComponents } from "@egovernments/digit-ui-module-hearings";
 import { initCasesComponents } from "@egovernments/digit-ui-module-cases";
 import { initHomeComponents } from "@egovernments/digit-ui-module-home";
 
-import "@egovernments/dristi-ui-css/dist/index.min.css";
-
 window.contextPath =
   window?.globalConfigs?.getConfig("CONTEXT_PATH") || "digit-ui";
 
