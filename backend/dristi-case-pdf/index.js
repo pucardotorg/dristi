d08eae6bed21587711df1c8805f28bcf107ff976
utpalBeehyv <utpal.kant@beehyv.com>
2025-02-12 15:00:56 +0530

    increase the payload size for fetchComplainPDF (#1752)

--- backend/dristi-case-pdf/index.js
+++ backend/dristi-case-pdf/index.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const caseRoutes = require('./routes/caseRoutes');
-const errorHandler = require('./middlewares/errorHandler');
+const bodyParser = require("body-parser");
+const caseRoutes = require("./routes/caseRoutes");
+const errorHandler = require("./middlewares/errorHandler");
+const config = require("./config/config");
 
-app.use(bodyParser.json());
-app.use('/dristi-case-pdf', caseRoutes);  // Updated context path
+const limitSize = config?.payloadSize;
+
+app.use(bodyParser.json({ limit: limitSize }));
+app.use(bodyParser.urlencoded({ limit: limitSize, extended: true }));
+app.use("/dristi-case-pdf", caseRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 8090;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
